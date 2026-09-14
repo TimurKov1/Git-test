510bf9ef4024adaf7a1dee76b103fabd9746d71f
Timur Kovalev <timur1711@icloud.com>
2026-09-14 19:41:41 +0300

    Добавил calculate_std

--- stats.py
+++ stats.py
@@ -1,2 +1,7 @@
 def calculate_mean(numbers):
     return sum(numbers) / len(numbers)
+
+def calculate_std(numbers):
+    mean = calculate_mean(numbers)
+    variance = sum((x - mean) ** 2 for x in numbers) / len(numbers)
+    return variance ** 0.5
